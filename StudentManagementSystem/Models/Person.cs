1673ce0a8c2769a51d264691aaba337ccf029279
Simran-Behera-01 <208r1a67b2@gmail.com>
2026-07-24 15:05:57 +0530

    Added Models.

--- StudentManagementSystem/Models/Person.cs
+++ StudentManagementSystem/Models/Person.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace StudentManagementSystem.Models
 {
-    internal class Person
+    internal abstract class Person
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        protected Person(int id, string firstName, string lastName, int age)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public abstract void DisplayInfo();
     }
 }
--- StudentManagementSystem/Models/Student.cs
+++ StudentManagementSystem/Models/Student.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace StudentManagementSystem.Models
 {
-    internal class Student
+    internal class Student : Person
     {
+        public int RollNumber { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public double Percentage { get; set; }
+        public Student(int id, string firstName, string lastName, int age, int rollNumber, string department, string email, string phoneNumber, double percentage) : base(id, firstName, lastName, age)
+        {
+            RollNumber = rollNumber;
+            Department = department;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Percentage = percentage;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Roll Number: {RollNumber}");
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Department: {Department}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Phone Number: {PhoneNumber}");
+            Console.WriteLine($"Percentage: {Percentage}");
+        }
     }
 }
