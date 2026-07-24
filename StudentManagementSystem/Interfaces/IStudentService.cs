99ed191ad6f9f3172a619abbbe712890e6ec3d8a
Simran-Behera-01 <208r1a67b2@gmail.com>
2026-07-24 15:58:04 +0530

    Created Service Interface.

--- StudentManagementSystem/Interfaces/IStudentService.cs
+++ StudentManagementSystem/Interfaces/IStudentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace StudentManagementSystem.Interfaces
 {
     internal interface IStudentService
     {
+        void AddStudent(Student student);
+        void UpdateStudent(Student updatedStudent);
+        void DeleteStudent(int rollNumber);
+        Student? GetStudentByRollNumber(int rollNumber);
+        List<Student> SearchStudentsByName(string name);
+        List<Student> GetAllStudents();
+        void SaveToFile(string filePath);
+        void LoadFromFile(string filePath);
     }
 }
